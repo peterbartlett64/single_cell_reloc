40e8c90db08fd970c41a2d0b033227c931b7f1d3
Peter Bartlett <69980231+peterbartlett64@users.noreply.github.com>
2024-11-26 12:27:35 -0500

    Delete tests directory

--- tests/Confirm_parquet_read.R
+++
@@ -1,8 +0,0 @@
-confirmation <- function(){
-  directory = "D:/Microfluidics/RESULTS_ALL/Most_final_collected/Combined_by_perc/Col_with_Abund/Merged"
-  dir(directory)
-  setwd(directory)
-  df = read_parquet("Final_wAbund.parquet")
-  head(df)
-  return(TRUE)
-}
--- tests/read_in_composite.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-import single_cell_reloc_paraquet.global_functions.global_variables as gv
-
-def readi
-
-if __name__ = "__main__"
-	gv.slash_switch(input("directory"))
